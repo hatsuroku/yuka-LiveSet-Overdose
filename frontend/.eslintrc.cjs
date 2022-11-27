94a766ab9251ed5264f413c4d683745bcf32376a
LiangXiaoshun <hatsuroku@foxmail.com>
2022-11-27 15:28:59 +0800

    添加 typescript 支持

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,28 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    // '@vue/eslint-config-prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
   },
-};
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    // '@typescript-eslint/camelcase': 'off',
+    // '@typescript-eslint/no-use-before-define': 'off',
+    // '@typescript-eslint/ban-ts-comment': 'off',
+    // '@typescript-eslint/ban-types': 'off',
+  },
+}
